90cc971aa3eb60f66b425936927d0740c777701c
S.H. Hayakawa <hayashu@post.j-parc.jp>
2020-06-23 18:21:24 +0900

    Update vme02_node

--- Frontend/vme02_node/src/userdevice.cc
+++ Frontend/vme02_node/src/userdevice.cc
@@ -13,7 +13,7 @@ namespace
 {
   vme::VmeManager& gVme = vme::VmeManager::GetInstance();
   const int max_polling   = 2000000;     //maximum count until time-out
-  const int max_try       = 100;         //maximum count to check data ready
+  const int max_try       = 100000;         //maximum count to check data ready
   const int max_data_size = 4*1024*1024; //maximum datasize by byte unit
   DaqMode g_daq_mode = DM_NORMAL;
 
@@ -57,12 +57,12 @@ open_device( NodeProp& nodeprop )
 
     // Zero suppression samples
     GEF_UINT32 n_lfwd[] = { 0x0a };
-    GEF_UINT32 n_lbk[] =  { 0x28 }; // 16bit shift to left
+    GEF_UINT32 n_lbk[] =  { 0x14 }; // 16bit shift to left
 
     // Zero suppression Threshold
     GEF_UINT32 zs_threshold[][vme::CaenV1724::NofCh] = 
       {
-	{15736, 15772, 15880, 15990, 15960, 15960, 15840, 15900}
+	{/*15736*/15460, 15772, 15880, 15990, 15960, 15960, 15840, 15900}
       };
     GEF_UINT32 zs_threshold_weight = 0; // 0:fine, 1:coarse.       30bit shift to left
     GEF_UINT32 zs_operation_logic  = 1; // 0:positive, 1:negative. 31bit shift to left
@@ -105,11 +105,14 @@ open_device( NodeProp& nodeprop )
 	m->WriteRegister( vme::CaenV1724::ZeroSuppSample + ch*vme::CaenV1724::IReg,
 			  (n_lfwd[i])       |
 			  (n_lbk[i]) << 16  );
+
       }// for(i)
 
       m->WriteRegister( vme::CaenV1724::MemoryAfullLv,   0x3ef  );
       usleep(10*1000);
-      m->WriteRegister( vme::CaenV1724::CustomSize,      0x4b   );
+      //m->WriteRegister( vme::CaenV1724::CustomSize,      0x4b   );
+      //m->WriteRegister( vme::CaenV1724::CustomSize,      0x5a   ); // 180 sample
+      m->WriteRegister( vme::CaenV1724::CustomSize,      0x8c   ); // 280 sample
       m->WriteRegister( vme::CaenV1724::IOCtrl,          0x40   );
       m->WriteRegister( vme::CaenV1724::GPOEnMask,       0x1<<30);
       m->WriteRegister( vme::CaenV1724::AcqCtrl,         0x24   );
@@ -132,7 +135,8 @@ open_device( NodeProp& nodeprop )
     reg = reg | vme::RMME::regFifoReset | vme::RMME::regInputReset | vme::RMME::regSerialReset;
     m->WriteRegister( vme::RMME::Control, reg );
     //    m->WriteRegister( vme::RMME::Pulse, 0x1 );
-    m->WriteRegister( vme::RMME::FifoDepth, 0x1d);
+    //m->WriteRegister( vme::RMME::FifoDepth, 0x1d);
+    m->WriteRegister( vme::RMME::FifoDepth, 0x3E8);
 
 #ifdef DebugPrint
     m->Print();
@@ -150,13 +154,15 @@ init_device( NodeProp& nodeprop )
   switch(g_daq_mode){
   case DM_NORMAL:
     {
-      {
+      {// V1724
 	const int n = gVme.GetNumOfModule<vme::CaenV1724>();
-	for( int i=0; i<n; ++i ){
-	  vme::CaenV1724* m = gVme.GetModule<vme::CaenV1724>(i);
-	  m->WriteRegister( vme::CaenV1724::SoftClear, 0x1 );
-	}
-      }
+	for(int i_try = 0; i_try<3; ++i_try){
+	  for( int i=0; i<n; ++i ){
+	    vme::CaenV1724* m = gVme.GetModule<vme::CaenV1724>(i);
+	    m->WriteRegister( vme::CaenV1724::SoftClear, 0x1 );
+	  }// for(i)
+	}// for(i_try)
+      }// V1724
 
       vme::RMME* m = gVme.GetModule<vme::RMME>(0);
       int reg = vme::RMME::regSelNIM4 | vme::RMME::regDaqGate;
@@ -211,8 +217,16 @@ wait_device( NodeProp& nodeprop )
       ////////// Polling
       int reg = 0;
       vme::RMME* m = gVme.GetModule<vme::RMME>(0);
+      //vme::CaenV1724* m_fadc = gVme.GetModule<vme::CaenV1724>(0);
+      //int dready = 0;
+
       for( int i=0; i<max_polling; ++i ){
 	reg = m->ReadRegister( vme::RMME::WriteCount );
+
+	std::cout << "RM reg(WriteCount) : " << reg << std::endl;
+	//dready = (m_fadc->ReadRegister( vme::CaenV1724::AcqStatus ) >> 3) & 0x1;
+
+	//if( ((reg & 0x3ff) != 0) && (dready == 1)) return 0; // FIFO is not empty
 	if( (reg & 0x3ff) != 0 ) return 0; // FIFO is not empty
       }
 
