919b3d1e9349e524e1dfdba7bcb926e7af936d2b
Rintaro Kurata <rintaro.kurata@gmail.com>
2026-09-04 01:09:43 +0900

    feat: migrate latest rayraw-node and related files from rayraw bench at Tohoku University

--- Frontend/rayraw_node/script/fe_start.sh
+++ Frontend/rayraw_node/script/fe_start.sh
@@ -5,28 +5,39 @@ bin_dir=$(dirname `readlink -f $0`)
 cd $bin_dir
 
 #$bin_dir/message.sh & #> /dev/null 2> /dev/null &
-#./message.sh > /dev/null 2> /dev/null &
+./message.sh > /dev/null 2> /dev/null &
 
 sleep 1
-#start hulRM
 
-#$HOME/HUL_Scaler/bin/set_nimio 192.168.10.62
+# #start hulRM
+# $HOME/HUL_Scaler/bin/set_nimio 192.168.10.62
 
-# enable mezzanine slot (0th value is dummy)
-en_slot=(0x0, 0x1, 0x3, 0x1, 0x3)
+# #enable mezzanine slot (0th value is dummy)
+# en_slot=(0x0, 0x1, 0x3, 0x1, 0x3)
 
-#start hulhrtdc
-for i in $(seq 1 1)
-#for i in $(seq 1 3)
-#for i in $(seq 2 2)
-#for i in $(seq 2 3)
+#start rayraw
+# for i in $(seq 1 1)
+for i in $(seq 2 2)
+# for i in $(seq 1 2)
 do
     nodeid=`expr $((0x100)) + $i`
     nickname=rayraw-`expr + $i`
-    dataport=`expr 9100 + $i`
-    sitcp_ip=192.168.10.`expr 15 + $i`
+    dataport=`expr 9000 + $i`
+    sitcp_ip=192.168.11.`expr 100 + $i`
+
+    # rayraw v3 ip
+    if [ "$i" -eq 2 ]; then
+       sitcp_ip=192.168.10.16
+    fi
+
+
+
+    # min_window=115
+    # max_window=165
+    # min_window=100
     min_window=100
-    max_window=1000
+    # max_window=600
+    max_window=200
 
     $bin_dir/frontend_rayraw.sh \
 	$nickname \
@@ -34,7 +45,6 @@ do
 	$dataport \
 	$sitcp_ip \
 	$min_window \
-	$max_window 
-#	>/dev/null 2>/dev/null &
-
+	$max_window \
+	>/dev/null 2>/dev/null &
 done
--- Frontend/rayraw_node/script/frontend_rayraw.sh
+++ Frontend/rayraw_node/script/frontend_rayraw.sh
@@ -2,7 +2,7 @@
 
 script_dir=$(dirname `readlink -f $0`)
 bin_dir=$script_dir/../bin
-frontend=rayraw_frontend
+frontend=frontend_rayraw
 
 if [ $# != 6 ]; then
     echo "Usage : $(basename $0) --nickname=NICK_NAME --nodeid=NODE_ID --data-port=DATA_PORT --sctcp-ip=SiTCP_IP --min-twin=MinWindow --max-twin=MaxWindow"
